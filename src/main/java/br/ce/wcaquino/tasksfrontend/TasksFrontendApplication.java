e50d82ead9496057da76b1f47448c30e05029ed7
joice.marek <joice.marek@shoppingbrasil.com.br>
2026-07-31 10:38:15 -0300

    Update Maven wrapper and dependencies; refactor main application class

--- src/main/java/br/ce/wcaquino/tasksfrontend/TasksFrontendApplication.java
+++ src/main/java/br/ce/wcaquino/tasksfrontend/TasksFrontendApplication.java
@@ -2,19 +2,12 @@ package br.ce.wcaquino.tasksfrontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksFrontendApplication extends SpringBootServletInitializer {
+public class TasksFrontendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksFrontendApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksFrontendApplication.class);
 	}
 
 }
